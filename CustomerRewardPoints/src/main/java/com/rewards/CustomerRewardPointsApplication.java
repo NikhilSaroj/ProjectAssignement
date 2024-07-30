36a3bfcb3ec1a6bb23e529e162e15d2a88271b12
nikhilshriniwas.s <nikhilshriniwas.s@PUNITP666757L.ad.infosys.com>
2024-07-30 19:43:43 +0530

    Initial commit

---
+++ CustomerRewardPoints/src/main/java/com/rewards/CustomerRewardPointsApplication.java
@@ -0,0 +1,11 @@
+package com.rewards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerRewardPointsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerRewardPointsApplication.class, args);
+	}
+}
---
+++ CustomerRewardPoints/src/main/java/com/rewards/exception/CustomerRewardPointException.java
@@ -0,0 +1,13 @@
+package com.rewards.exception;
+
+//User Defined/Customized exception class 
+public class CustomerRewardPointException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public CustomerRewardPointException(String message) {
+		super(message);
+		
+	}
+	
+}
